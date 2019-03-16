96d67e6cf0147202a54a0e8cc6d960fb9f013698
gness1804 <graham.nessler@gmail.com>
2019-03-15 20:07:01 -0500

    Contrived example of simple useReducer to increment a counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import { UserContext } from './index';
 
+const initState = {
+  count: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {
+        count: state.count + 1,
+      };
+
+    default:
+      return initState;
+  }
+};
+
 const App = () => {
+  const [resultState, dispatch] = useReducer(reducer, initState);
   const value = useContext(UserContext);
 
   return (
     <>
-      <div>Hello, {value}</div>
+      <p>Count: {resultState.count}</p>
+      <button
+        className="border p-1"
+        onClick={() => dispatch({ type: 'increment' })}
+      >
+        Increment
+      </button>
     </>
   );
 };
