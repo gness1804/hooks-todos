bf04c077f0e5feb0e83105f7901bce52e0c7d266
gness1804 <graham.nessler@gmail.com>
2019-03-15 20:36:16 -0500

    Add decrement and reset actions

--- src/App.js
+++ src/App.js
@@ -11,10 +11,27 @@ const reducer = (state, action) => {
       return {
         count: state.count + 1,
       };
-
+    case 'decrement':
+      return {
+        count: state.count - 1,
+      };
+    case 'reset':
+      return initState;
     default:
       return initState;
   }
+};
+
+const incrementAction = {
+  type: 'increment',
+};
+
+const decrementAction = {
+  type: 'decrement',
+};
+
+const resetAction = {
+  type: 'reset',
 };
 
 const App = () => {
@@ -25,10 +42,19 @@ const App = () => {
     <>
       <p>Count: {resultState.count}</p>
       <button
-        className="border p-1"
-        onClick={() => dispatch({ type: 'increment' })}
+        className="border m-1 p-1"
+        onClick={() => dispatch(incrementAction)}
       >
         Increment
+      </button>
+      <button
+        className="border m-1 p-1"
+        onClick={() => dispatch(decrementAction)}
+      >
+        Decrement
+      </button>
+      <button className="border m-1 p-1" onClick={() => dispatch(resetAction)}>
+        Reset
       </button>
     </>
   );
