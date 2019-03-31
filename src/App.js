490a8943ab64ea3f5e1745dfc96798f2c1af1c44
gness1804 <graham.nessler@gmail.com>
2019-03-31 11:35:44 -0500

    Create custom useAPI hook to fetch todos API data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+// prettier-ignore
 /* eslint-disable-next-line no-unused-vars */
-import React, { useContext, useReducer } from 'react';
+import React, {
+  useContext,
+  useReducer,
+  useState,
+  useEffect,
+} from 'react';
+import axios from 'axios';
 import TodosContext from './context';
 import reducer from './reducer';
 /* eslint-disable-next-line no-unused-vars */
@@ -7,10 +14,31 @@ import TodoList from './components/TodoList';
 /* eslint-disable-next-line no-unused-vars */
 import TodoForm from './components/TodoForm';
 
+const useAPI = endpoint => {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const result = await axios.get(endpoint);
+      setData(result.data);
+    } catch (err) {
+      throw new Error(`Error fetching data: ${err}`);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return data;
+};
+
 const App = () => {
   const initState = useContext(TodosContext);
   const [state, dispatch] = useReducer(reducer, initState);
 
+  const todosEndpoint = 'https://hooks-api.grahamnessler.now.sh/data.json';
+  const savedTodos = useAPI(todosEndpoint);
+  // TODO: use the reducer to set the state.todos on line 38 to the savedTodos.
   return (
     <TodosContext.Provider value={{ state, dispatch }}>
       <TodoList />
--- src/context.js
+++ src/context.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import todos from './data/todos';
 
 const TodosContext = React.createContext({
-  todos,
+  todos: [],
   currentTodo: {},
 });
 
